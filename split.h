c2d27a7481cf2a44eb03eb5ac8fe4e158d4eccb3
Jun <smilent_sj@163.com>
2015-12-25 10:28:16 +0800

    add a function for splitting strings
    add a function:
    vector<string> split(const string & s, const string & delim)

---
+++ split.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+// s: the string that needs splitting.
+// delim: the delimeters. Each containing charater(not the whole delim) is used to split s.
+// return a vector, which contains all the tokens
+vector<string> split(const string& s, const string & delim){
+    //char* pch = strtok(strdup(s.c_str()), delim.c_str());
+    char* pch = strtok(const_cast<char*>(s.c_str()), delim.c_str());
+    vector<string> tokens;
+    while(pch != NULL){
+        string str(pch);
+        tokens.push_back(str);
+        pch = strtok(NULL, delim.c_str());
+    }
+    
+    //free(pch); // must be called if strdup is used
+    return tokens;
+}
+
